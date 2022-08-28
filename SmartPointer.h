7f2a28a8ecdf5b5797167c501a79f9035cd91516
Mihail Terziev <100141310+MihailTerziev@users.noreply.github.com>
2022-08-28 22:46:05 +0300

    Add files via upload
    Redefined smart pointer class.

--- SmartPointer.h
+++ SmartPointer.h
@@ -1,97 +1,200 @@
-#ifndef SMART_POINTER_H
-#define SMART_POINTER_H
+#ifndef SMART_POINTER_H_
+#define SMART_POINTER_H_
 
-#include <cstddef>
 #include <stdexcept>
-#include <algorithm>
+
 
 template<typename T> class SmartPointer {
-    enum PointerType {
-        VALUE,
-        ARRAY,
-        NULLP
-    };
+    private:
+        enum PointerType {
+            ALLOC,
+            NULLP
+        };
 
-    T* pointer;
-    PointerType type;
-    size_t size;
+        T* mPtr;
+        PointerType mType;
+        bool mIsCopy;
+
+        void __copy(const SmartPointer& other) {
+            mPtr = other.mPtr;
+            mType = other.mType;
+            mIsCopy = other.mIsCopy;
+        }
+
+        void __reset(SmartPointer& sp) {
+            sp.mPtr = nullptr;
+            sp.mType = PointerType::NULLP;
+        }
+
+        void __move(SmartPointer& other) {
+            __copy(other);
+            __reset(other);
+        }
+
+        void __dealloc() {
+            if (mPtr != nullptr)
+			    delete mPtr;
+        }
 
     public:
-        SmartPointer() : 
-            pointer(nullptr), type(PointerType::NULLP), size(0) {}
+        SmartPointer() :
+            mPtr(nullptr), mType(PointerType::NULLP), mIsCopy(false) {}
 
-        SmartPointer(const T& value) : 
-            pointer(new T(value)), type(PointerType::VALUE), size(1) {}
+        SmartPointer(T* heap_alloc_mem_) :
+            mPtr(heap_alloc_mem_), mType(PointerType::ALLOC), mIsCopy(false) {}
 
-        SmartPointer(T* array, const size_t& size_) : 
-            pointer(array), type(PointerType::ARRAY), size(size_) {}
-
-        SmartPointer(const SmartPointer& other) : 
-            pointer(nullptr), type(other.type), size(other.size) 
-            {   
-                if (type == PointerType::VALUE)
-                    pointer = new T(*other.pointer);
-
-                else if (type == PointerType::ARRAY)
-                    std::copy(other.pointer, other.pointer + size, pointer);
-            }
+        SmartPointer(std::nullptr_t) :
+            mPtr(nullptr), mType(PointerType::NULLP), mIsCopy(false) {}
 
         ~SmartPointer() {
-            if (type == PointerType::VALUE)
-                delete pointer;
-
-            else if (type == PointerType::ARRAY)
-                delete[] pointer;
+            if (!mIsCopy)
+                __dealloc();
         }
 
-        SmartPointer(SmartPointer &&other) = delete;
-        SmartPointer& operator=(SmartPointer &&other) = delete;
+        SmartPointer(const SmartPointer& other) : mPtr(other.mPtr), mType(other.mType), mIsCopy(true) {}
 
-        friend void swap(SmartPointer& first, SmartPointer& second) {
-            std::swap(first.pointer, second.pointer);
-            std::swap(first.type, second.type);
-            std::swap(first.size, second.size);
+        SmartPointer& operator=(const SmartPointer& other) {
+            if (this != &other) {
+                __copy(other);
+            
+                if (!mIsCopy)
+                    mIsCopy = true;
+            }
+
+            return *this;
         }
 
-        SmartPointer& operator=(SmartPointer other) {
-            swap(*this, other);
+        SmartPointer& operator=(std::nullptr_t) {
+            __reset(*this);
+            return *this;
+        }
+
+        SmartPointer(SmartPointer&& other) {
+            __move(other);
+        }
+
+        SmartPointer& operator=(SmartPointer&& other) {
+            if (this != &other) {
+                __dealloc();
+                __move(other);
+            }
+            
             return *this;
         }
 
         T* operator->() { 
-            if (type == PointerType::NULLP)
+            if (mType == PointerType::NULLP)
                 throw std::runtime_error("Reference_Error: Nullptr doesn't point to anything!");
 
-            return pointer; 
+            return mPtr;
         }
 
         T& operator*() { 
-            if (type == PointerType::NULLP)
+            if (mType == PointerType::NULLP)
                 throw std::runtime_error("Reference_Error: Can't dereference nullptr!");
 
-            return *pointer;
-        }
-
-        T& operator[](const size_t& index) {
-            if (type == PointerType::NULLP)
-                throw std::runtime_error("Reference_Error: Can't access nullptr!");
-
-            else if (type == PointerType::VALUE)
-                throw std::runtime_error("Reference_Error: Pointer to single value!");
-
-            else if (index < 0 || index >= size)
-                throw std::out_of_range("Memory_Access_Violation: Index out of range!");
-
-            return pointer[index];
-        }
-
-        void clear() {
-            if (pointer != nullptr) {
-                pointer = nullptr;
-                type = PointerType::NULLP;
-                size = 0;
-            }
+            return *mPtr;
         }
 };
 
-#endif
+template<typename T> class SmartPointer<T[]> {
+    private:
+        enum PointerType {
+            ALLOC,
+            NULLP
+        };
+
+        T* mPtr;
+        PointerType mType;
+        bool mIsCopy;
+
+        void __copy(const SmartPointer& other) {
+            mPtr = other.mPtr;
+            mType = other.mType;
+            mIsCopy = other.mIsCopy;
+        }
+
+        void __reset(SmartPointer& sp) {
+            sp.mPtr = nullptr;
+            sp.mType = PointerType::NULLP;
+        }
+
+        void __move(SmartPointer& other) {
+            __copy(other);
+            __reset(other);
+        }
+
+        void __dealloc() {
+            if (mPtr != nullptr)
+			    delete[] mPtr;
+        }
+
+    public:
+        SmartPointer() :
+            mPtr(nullptr), mType(PointerType::NULLP), mIsCopy(false) {}
+
+        SmartPointer(T* heap_alloc_mem_) :
+            mPtr(heap_alloc_mem_), mType(PointerType::ALLOC), mIsCopy(false) {}
+
+        SmartPointer(std::nullptr_t) :
+            mPtr(nullptr), mType(PointerType::NULLP), mIsCopy(false) {}
+
+        ~SmartPointer() {
+            if (!mIsCopy)
+                __dealloc();
+        }
+
+        SmartPointer(const SmartPointer& other) : mPtr(other.mPtr), mType(other.mType), mIsCopy(true) {}
+
+        SmartPointer& operator=(const SmartPointer& other) {
+            if (this != &other) {
+                __copy(other);
+            
+                if (!mIsCopy)
+                    mIsCopy = true;
+            }
+
+            return *this;
+        }
+
+        SmartPointer& operator=(std::nullptr_t) {
+            __reset(*this);
+            return *this;
+        }
+
+        SmartPointer(SmartPointer&& other) {
+            __move(other);
+        }
+
+        SmartPointer& operator=(SmartPointer&& other) {
+            if (this != &other) {
+                __dealloc();
+                __move(other);
+            }
+            
+            return *this;
+        }
+
+        T* operator->() { 
+            if (mType == PointerType::NULLP)
+                throw std::runtime_error("Reference_Error: Nullptr doesn't point to anything!");
+
+            return mPtr;
+        }
+
+        T& operator*() { 
+            if (mType == PointerType::NULLP)
+                throw std::runtime_error("Reference_Error: Can't dereference nullptr!");
+
+            return *mPtr;
+        }    
+
+        T& operator[](const int& index) {
+		    if(index < 0)
+		        throw std::runtime_error("Memory_Access_Violation: Negative index out of bound!");
+
+		    return mPtr[index];
+	    }
+};
+
+#endif  // SMART_POINTER_H_
